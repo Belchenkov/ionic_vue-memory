b07b853b8f6e696615654e1141bfdfdfe3b69a95
Aleksey Belchenkov <u608110@gmail.com>
2020-12-03 23:36:52 +0300

    First Styles

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>
+          <img src="https://img.icons8.com/cute-clipart/34/000000/copybook.png" />
+          All Memories
+        </ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-item>1</ion-item>
+        <ion-item>2</ion-item>
+        <ion-item>3</ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonContent,
+    IonToolbar,
+    IonList,
+    IonItem
+} from "@ionic/vue";
+
+export default {
+  name: "MemoriesPage",
+  components: {
+    IonContent,
+    IonTitle,
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonList,
+    IonItem
+  }
+}
+</script>
+
+<style scoped>
+  ion-title {
+    font-weight: bold;
+    font-family: 'Ultra', serif;
+  }
+</style>
